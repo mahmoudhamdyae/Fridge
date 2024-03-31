b2c71be65dbab2e62138f6e3101eff91e21a81b0
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-03-31 23:25:59 +0200

    Home Screen Bottom Navigation Bar

--- lib/home/presentation/screens/main_screen.dart
+++ lib/home/presentation/screens/main_screen.dart
@@ -10,6 +10,128 @@ import '../../../core/resources/app_strings.dart';
 import 'home_screen.dart';
 
 class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  static TextStyle optionStyle = getSmallStyle(fontSize: 14.0);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    LoginScreen(),
+    RegisterScreen(),
+    HomeScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.only(
+        //       topRight: Radius.circular(30.0),
+        //       topLeft: Radius.circular(30.0)
+        //   ),
+        //   boxShadow: [
+        //     BoxShadow(color: Colors.black26, blurRadius: 8, spreadRadius: 4),
+        //   ],
+        // ),
+        decoration: BoxDecoration(
+    // borderRadius: BorderRadius.only(
+    //       topRight: Radius.circular(30.0),
+    //       topLeft: Radius.circular(30.0)
+    //   ),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 8,
+              spreadRadius: 4
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 0
+                  ? Image.asset(
+                      AppAssets.homeSelected,
+                      width: 20,
+                      height: 20,
+                    )
+                  : Image.asset(
+                      AppAssets.home,
+                      width: 20,
+                      height: 20,
+                    ),
+              label: AppStrings.bottomBarMain,
+            ),
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 1
+                  ? Image.asset(
+                      AppAssets.clientsSelected,
+                      width: 20,
+                      height: 20,
+                    )
+                  : Image.asset(
+                      AppAssets.clients,
+                      width: 20,
+                      height: 20,
+                    ),
+              label: AppStrings.bottomBarClients,
+            ),
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 2
+                  ? Image.asset(
+                      AppAssets.fridgeSelected,
+                      width: 20,
+                      height: 20,
+                    )
+                  : Image.asset(
+                      AppAssets.fridge,
+                      width: 20,
+                      height: 20,
+                    ),
+              label: AppStrings.bottomBarFridge,
+            ),
+            BottomNavigationBarItem(
+              icon: _selectedIndex == 3
+                  ? Image.asset(
+                      AppAssets.reportsSelected,
+                      width: 20,
+                      height: 20,
+                    )
+                  : Image.asset(
+                      AppAssets.reports,
+                      width: 20,
+                      height: 20,
+                    ),
+              label: AppStrings.bottomBarReports,
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color(0xff2D5AAF),
+          unselectedItemColor: AppColors.dark3,
+          onTap: _onItemTapped,
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class MainScreen extends StatefulWidget {
 
   final int selectedIndex;
   const MainScreen({super.key, this.selectedIndex = 0});
@@ -139,4 +261,4 @@ class _MainScreenState extends State<MainScreen> {
       ),
     );
   }
-}
+}*/
