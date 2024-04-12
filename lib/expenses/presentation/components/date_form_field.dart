6b88dfbcacd7b489bc3b9d4d7207dbc70d5e8540
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-04-12 14:23:55 +0200

    Date

--- lib/expenses/presentation/components/date_form_field.dart
+++ lib/expenses/presentation/components/date_form_field.dart
@@ -4,7 +4,7 @@ import 'package:fridge/core/resources/styles_manager.dart';
 import '../../../core/resources/app_strings.dart';
 import '../../../core/utils/validate_operations.dart';
 
-class DateFormField extends StatelessWidget {
+class DateFormField extends StatefulWidget {
 
   final TextEditingController dateController;
   const DateFormField({
@@ -12,6 +12,67 @@ class DateFormField extends StatelessWidget {
     required this.dateController,
   });
 
+  @override
+  State<DateFormField> createState() => _DateFormFieldState();
+}
+
+class _DateFormFieldState extends State<DateFormField>
+    with RestorationMixin {
+
+  @override
+  String? get restorationId => 'main';
+
+  final RestorableDateTime _selectedDate =
+  RestorableDateTime(DateTime.now());
+  late final RestorableRouteFuture<DateTime?> _restorableDatePickerRouteFuture =
+  RestorableRouteFuture<DateTime?>(
+    onComplete: _selectDate,
+    onPresent: (NavigatorState navigator, Object? arguments) {
+      return navigator.restorablePush(
+        _datePickerRoute,
+        arguments: _selectedDate.value.millisecondsSinceEpoch,
+      );
+    },
+  );
+
+  @pragma('vm:entry-point')
+  static Route<DateTime> _datePickerRoute(
+      BuildContext context,
+      Object? arguments,
+      ) {
+    return DialogRoute<DateTime>(
+      context: context,
+      builder: (BuildContext context) {
+        return DatePickerDialog(
+          restorationId: 'date_picker_dialog',
+          initialEntryMode: DatePickerEntryMode.calendarOnly,
+          initialDate: DateTime.fromMillisecondsSinceEpoch(arguments! as int),
+          firstDate: DateTime(DateTime.now().year - 10),
+          lastDate: DateTime(DateTime.now().year + 10),
+        );
+      },
+    );
+  }
+
+  @override
+  void restoreState(RestorationBucket? oldBucket, bool initialRestore) {
+    registerForRestoration(_selectedDate, 'selected_date');
+    registerForRestoration(
+        _restorableDatePickerRouteFuture, 'date_picker_route_future');
+  }
+
+  void _selectDate(DateTime? newSelectedDate) {
+    if (newSelectedDate != null) {
+      setState(() {
+        _selectedDate.value = newSelectedDate;
+        int year = _selectedDate.value.year;
+        String month = _selectedDate.value.month < 10 ? '0${_selectedDate.value.month}' : '${_selectedDate.value.month}';
+        String day = _selectedDate.value.day < 10 ? '0${_selectedDate.value.day}' : '${_selectedDate.value.day}';
+        debugPrint('Date = $year-$month-$day');
+        widget.dateController.text = '$year-$month-$day';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +80,10 @@ class DateFormField extends StatelessWidget {
       decoration: getFilledTextFieldDecoration(
         hint: AppStrings.expensesScreenDateHint,
       ),
-      controller: dateController,
+      onTap: () {
+        _restorableDatePickerRouteFuture.present();
+      },
+      controller: widget.dateController,
       textInputAction: TextInputAction.next,
       keyboardType: TextInputType.text,
       validator: (value) => ValidateOperations.normalValidation(value),
