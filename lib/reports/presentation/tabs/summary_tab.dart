54030d23ff8ee506bae83ba083ab8cbc5323cfeb
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-04-14 13:33:02 +0200

    Summary UI

--- lib/reports/presentation/tabs/summary_tab.dart
+++ lib/reports/presentation/tabs/summary_tab.dart
@@ -1,4 +1,6 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fridge/core/extensions/context_extension.dart';
 import 'package:fridge/core/extensions/num_extensions.dart';
 import 'package:fridge/core/resources/app_colors.dart';
 import 'package:fridge/core/resources/app_strings.dart';
@@ -16,7 +18,7 @@ class SummaryTab extends StatelessWidget {
       children: [
         16.ph,
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -28,6 +30,7 @@ class SummaryTab extends StatelessWidget {
                     color: AppColors.secondary
                   ),
                 ),
+                Container(width: context.dynamicWidth(.5),),
                 Text(
                   AppStrings.reportsTabIncome,
                   style: getSmallStyle(
@@ -55,10 +58,199 @@ class SummaryTab extends StatelessWidget {
                   ),
                 ),
               ],
-            )
+            ),
           ],
+        ),
+        32.ph,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start  ,
+          children: [
+            Text(
+              AppStrings.reportsTabTotalPrice,
+              style: getSmallStyle(
+                  fontSize: 12.0,
+                  color: AppColors.dark2
+              ),
+            ),
+            Text(
+              '187.000 ${AppStrings.egp}',
+              style: getSmallStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeightManager.semiBold,
+                  color: const Color(0xff193263)
+              ),
+            ),
+          ],
+        ),
+        16.ph,
+        SizedBox(
+          height: 200,
+          child: LineChart(
+            sampleData1,
+            duration: const Duration(milliseconds: 250),
+          ),
         )
       ],
     );
   }
+
+  LineChartData get sampleData1 => LineChartData(
+    gridData: gridData,
+    titlesData: titlesData1,
+    borderData: borderData,
+    lineBarsData: lineBarsData1,
+    minX: 0,
+    maxX: 14,
+    maxY: 4,
+    minY: 0,
+  );
+
+  FlTitlesData get titlesData1 => FlTitlesData(
+    bottomTitles: AxisTitles(
+      sideTitles: bottomTitles,
+    ),
+    rightTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    topTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    leftTitles: AxisTitles(
+      sideTitles: leftTitles(),
+    ),
+  );
+
+  List<LineChartBarData> get lineBarsData1 => [
+    lineChartBarData1_2,
+    lineChartBarData1_1,
+  ];
+
+  Widget leftTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 14,
+    );
+    String text;
+    switch (value.toInt()) {
+      case 1:
+        text = '1m';
+        break;
+      case 2:
+        text = '2m';
+        break;
+      case 3:
+        text = '3m';
+        break;
+      case 4:
+        text = '5m';
+        break;
+      case 5:
+        text = '6m';
+        break;
+      default:
+        return Container();
+    }
+
+    return Text(text, style: style, textAlign: TextAlign.center);
+  }
+
+  SideTitles leftTitles() => SideTitles(
+    getTitlesWidget: leftTitleWidgets,
+    showTitles: true,
+    interval: 1,
+    reservedSize: 40,
+  );
+
+  Widget bottomTitleWidgets(double value, TitleMeta meta) {
+    const style = TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 16,
+    );
+    Widget text;
+    switch (value.toInt()) {
+      case 2:
+        text = const Text('SEPT', style: style);
+        break;
+      case 7:
+        text = const Text('OCT', style: style);
+        break;
+      case 12:
+        text = const Text('DEC', style: style);
+        break;
+      default:
+        text = const Text('');
+        break;
+    }
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 10,
+      child: text,
+    );
+  }
+
+  SideTitles get bottomTitles => SideTitles(
+    showTitles: true,
+    reservedSize: 32,
+    interval: 1,
+    getTitlesWidget: bottomTitleWidgets,
+  );
+
+  FlGridData get gridData => const FlGridData(show: false);
+
+  FlBorderData get borderData => FlBorderData(
+    show: true,
+    border: const Border(
+      bottom: BorderSide(color: Colors.transparent),
+      left: BorderSide(color: Colors.transparent),
+      right: BorderSide(color: Colors.transparent),
+      top: BorderSide(color: Colors.transparent),
+    ),
+  );
+
+  LineChartBarData get lineChartBarData1_1 => LineChartBarData(
+    isCurved: true,
+    color: AppColors.secondary,
+    barWidth: 2,
+    isStrokeCapRound: true,
+    dotData: const FlDotData(show: false),
+    belowBarData: BarAreaData(show: false),
+    spots: const [
+      FlSpot(1, 1),
+      FlSpot(3, 1.5),
+      FlSpot(5, 1.4),
+      FlSpot(7, 3.4),
+      FlSpot(10, 2),
+      FlSpot(12, 2.2),
+      FlSpot(13, 1.8),
+    ],
+  );
+
+  LineChartBarData get lineChartBarData1_2 => LineChartBarData(
+    isCurved: true,
+    color: const Color(0xff193263),
+    barWidth: 2,
+    isStrokeCapRound: true,
+    dotData: const FlDotData(show: false),
+    belowBarData: BarAreaData(
+      show: true,
+      color: AppColors.colorRamps3,
+      gradient: const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+          colors: [
+            Color(0xff6F98E7),
+            Color(0xffFFFFFF),
+          ]
+      )
+    ),
+    spots: const [
+      FlSpot(1, 1),
+      FlSpot(3, 2.8),
+      FlSpot(7, 1.2),
+      FlSpot(10, 2.8),
+      FlSpot(12, 2.6),
+      FlSpot(13, 3.9),
+    ],
+  );
 }
