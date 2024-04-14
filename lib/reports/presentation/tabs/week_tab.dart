e6404ca19950d7a8137ab1d2d00470757c341170
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-04-14 11:15:40 +0200

    Week UI

--- lib/reports/presentation/tabs/week_tab.dart
+++ lib/reports/presentation/tabs/week_tab.dart
@@ -1,10 +1,272 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fridge/core/extensions/num_extensions.dart';
+import 'package:fridge/core/resources/app_colors.dart';
+import 'package:fridge/core/resources/app_strings.dart';
+import 'package:fridge/core/resources/styles_manager.dart';
 
 class WeekTab extends StatelessWidget {
   const WeekTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
+      children: [
+        16.ph,
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: const BoxDecoration(
+            color: AppColors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            boxShadow: [
+              BoxShadow(
+                color: AppColors.grey,
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 0), // changes position of shadow
+              ),
+            ]
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'ssss ${AppStrings.egp}',
+                style: getLargeStyle(
+                  fontSize: 20,
+                  color: const Color(0xff193263)
+                ),
+              ),
+              Text(
+                AppStrings.reportsTabTotalPrice,
+                style: getSmallStyle(
+                  color: const Color(0xff6B6B6B),
+                  fontSize: 12,
+                ),
+              ),
+              16.ph,
+              SizedBox(
+                height: 200,
+                child: BarChart(
+                  BarChartData(
+                    barTouchData: barTouchData,
+                    titlesData: titlesData,
+                    borderData: borderData,
+                    barGroups: barGroups,
+                    gridData: const FlGridData(show: false),
+                    alignment: BarChartAlignment.spaceAround,
+                    maxY: 20,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        16.ph,
+        Container(
+          padding: const EdgeInsets.all(16.0),
+          decoration: const BoxDecoration(
+              color: AppColors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.grey,
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: Offset(0, 0), // changes position of shadow
+                ),
+              ]
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'ssss ${AppStrings.egp}',
+                style: getLargeStyle(
+                    fontSize: 20,
+                    color: const Color(0xff193263)
+                ),
+              ),
+              Text(
+                AppStrings.reportsTabExpenses,
+                style: getSmallStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
+
+  BarTouchData get barTouchData => BarTouchData(
+    enabled: false,
+    touchTooltipData: BarTouchTooltipData(
+      getTooltipColor: (group) => Colors.transparent,
+      tooltipPadding: EdgeInsets.zero,
+      tooltipMargin: 8,
+      getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+          ) {
+        return BarTooltipItem(
+          rod.toY.round().toString(),
+          const TextStyle(
+            color: AppColors.secondary,
+            fontWeight: FontWeight.bold,
+          ),
+        );
+      },
+    ),
+  );
+
+  Widget getTitles(double value, TitleMeta meta) {
+    TextStyle style = getSmallStyle(
+      color: AppColors.dark2,
+      fontSize: 12.0
+    );
+    String text;
+    switch (value.toInt()) {
+      case 0:
+        text = AppStrings.reportsTabFri;
+        break;
+      case 1:
+        text = AppStrings.reportsTabThu;
+        break;
+      case 2:
+        text = AppStrings.reportsTabWed;
+        break;
+      case 3:
+        text = AppStrings.reportsTabTue;
+        break;
+      case 4:
+        text = AppStrings.reportsTabMon;
+        break;
+      case 5:
+        text = AppStrings.reportsTabSun;
+        break;
+      case 6:
+        text = AppStrings.reportsTabSat;
+        break;
+      default:
+        text = '';
+        break;
+    }
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 4,
+      child: Text(text, style: style),
+    );
+  }
+
+  FlTitlesData get titlesData => FlTitlesData(
+    show: true,
+    bottomTitles: AxisTitles(
+      sideTitles: SideTitles(
+        showTitles: true,
+        reservedSize: 30,
+        getTitlesWidget: getTitles,
+      ),
+    ),
+    leftTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    topTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+    rightTitles: const AxisTitles(
+      sideTitles: SideTitles(showTitles: false),
+    ),
+  );
+
+  FlBorderData get borderData => FlBorderData(
+    show: false,
+  );
+
+  LinearGradient get _barsGradient => const LinearGradient(
+    colors: [
+      AppColors.primary,
+      AppColors.secondary,
+    ],
+    begin: Alignment.bottomCenter,
+    end: Alignment.topCenter,
+  );
+
+  List<BarChartGroupData> get barGroups => [
+    BarChartGroupData(
+      x: 0,
+      barRods: [
+        BarChartRodData(
+          toY: 8,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 1,
+      barRods: [
+        BarChartRodData(
+          toY: 10,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 2,
+      barRods: [
+        BarChartRodData(
+          toY: 14,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 3,
+      barRods: [
+        BarChartRodData(
+          toY: 15,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 4,
+      barRods: [
+        BarChartRodData(
+          toY: 13,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 5,
+      barRods: [
+        BarChartRodData(
+          toY: 10,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+    BarChartGroupData(
+      x: 6,
+      barRods: [
+        BarChartRodData(
+          toY: 16,
+          gradient: _barsGradient,
+        )
+      ],
+      showingTooltipIndicators: [0],
+    ),
+  ];
 }
