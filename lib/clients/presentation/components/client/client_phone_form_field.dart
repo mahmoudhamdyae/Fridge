18b1c192b1890c284b421918fe1c01ef390e9a0a
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-04-15 14:58:53 +0200

    Get Numbers from Phone

--- lib/clients/presentation/components/client/client_phone_form_field.dart
+++ lib/clients/presentation/components/client/client_phone_form_field.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:fridge/core/resources/styles_manager.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 import '../../../../core/resources/app_strings.dart';
-import '../../../../core/utils/validate_operations.dart';
+import '../../../../core/resources/styles_manager.dart';
+import '../../../domain/entities/contact.dart';
 
-class ClientPhoneFormField extends StatelessWidget {
+class ClientPhoneFormField extends StatefulWidget {
 
   final TextEditingController clientPhoneController;
   const ClientPhoneFormField({
@@ -12,17 +14,89 @@ class ClientPhoneFormField extends StatelessWidget {
     required this.clientPhoneController,
   });
 
+  @override
+  State<ClientPhoneFormField> createState() => _ClientPhoneFormFieldState();
+}
+
+class _ClientPhoneFormFieldState extends State<ClientPhoneFormField> {
+
+  List<String> names = [];
+  List<String> phones = [];
+  List<CustomContact> customContacts = [];
+
+  String selectedName = AppStrings.addClientScreenClientPhoneLabel;
+
+  @override
+  void initState() {
+    super.initState();
+    getContacts();
+  }
+
+  Future<void> getContacts() async {
+    if (await FlutterContacts.requestPermission()) {
+      // Get all contacts (lightly fetched)
+      List<Contact> contacts = await FlutterContacts.getContacts();
+
+      // Get all contacts (fully fetched)
+      contacts =
+      await FlutterContacts.getContacts(withProperties: true, withPhoto: true);
+
+      for (var element in contacts) {
+        // Get contact with specific ID (fully fetched)
+        Contact? contact = await FlutterContacts.getContact(element.id);
+        contact?.phones.forEach((phone) {
+          String customName = '${contact.name.first} ${contact.name.last}';
+          String customPhone = phone.number;
+          customContacts.add(CustomContact(name: customName, phone: customPhone));
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
+    return
+
+
+      TypeAheadField<CustomContact>(
+        suggestionsCallback: (search) {
+          return customContacts.where((contact) {
+            return contact.name.toLowerCase().contains(search.toLowerCase()) ||
+                contact.phone.toLowerCase().contains(search.toLowerCase());
+          }).toList();
+        },
+        builder: (context, controller, focusNode) {
+          return TextField(
+            controller: widget.clientPhoneController,
+            focusNode: focusNode,
+            decoration: getFilledTextFieldDecorationWithLabel(
+              label: AppStrings.addClientScreenClientPhoneLabel,
+            ),
+          );
+        },
+        itemBuilder: (context, city) {
+          return ListTile(
+            title: Text(city.name),
+            subtitle: Text(city.phone),
+          );
+        },
+        onSelected: (contact) {
+          widget.clientPhoneController.text = contact.phone;
+        },
+      )
+
+
+
+
+
+      /*TextFormField(
       decoration: getFilledTextFieldDecorationWithLabel(
         label: AppStrings.addClientScreenClientPhoneLabel,
       ),
-      controller: clientPhoneController,
+      controller: widget.clientPhoneController,
       textInputAction: TextInputAction.next,
       keyboardType: TextInputType.number,
       validator: (value) => ValidateOperations.normalValidation(value),
-    );
+    )*/;
   }
 }
