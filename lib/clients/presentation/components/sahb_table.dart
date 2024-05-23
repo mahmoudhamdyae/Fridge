dfe4de0611bc70862e08642ba69da64a2bffd3fb
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-05-23 11:20:08 +0300

    Get Amount Paid UI

--- lib/clients/presentation/components/sahb_table.dart
+++ lib/clients/presentation/components/sahb_table.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../../../core/resources/app_colors.dart';
-
 class SahbTable extends StatelessWidget {
   const SahbTable({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: AppColors.secondary, width: double.infinity, height: 150,);
+    return Container();
   }
 }
